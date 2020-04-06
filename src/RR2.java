0db3c2975e891262fcdcc5c45dc5b2254ecbed92
Fabian Dean <fabian@fabiandean.dev>
2020-04-05 18:19:21 -0700

    Finished simulation support for all algorithms

--- src/RR2.java
+++ src/RR2.java
@@ -2,7 +2,7 @@
 /**
  * Fabian Flores
  * Date created: 03/24/2020
- * Last modified: 03/26/2020
+ * Last modified: 04/05/2020
  * Table output aided by code found here ->
  * https://www.logicbig.com/how-to/code-snippets/jcode-java-cmd-command-line-table.html
  */
@@ -14,8 +14,7 @@ public class RR2 {
     public RR2() {
     }
 
-    public static void main(String[] args) {
-        File file;
+    public static double rr2(File file, int n) {
         Scanner in;
         CommandLineTable table = new CommandLineTable();
         ArrayList<String> processNames = new ArrayList<>();
@@ -26,15 +25,21 @@ public class RR2 {
         boolean done = false;
 
         try {
-            file = new File(args[0]);
             in = new Scanner(file);
             table.setShowVerticalLines(true);
             table.setHeaders("Job #", "Start time", "End time", "Job completion");
-
-            // save process names and their respective process times
-            while (in.hasNextLine()) {
-                processNames.add(in.nextLine());
-                processTimes.add(Integer.parseInt(in.nextLine()));
+            // based on the input data size selected by user
+            if (n == 0) {
+                // save process names and their respective process times
+                while (in.hasNextLine()) {
+                    processNames.add(in.nextLine());
+                    processTimes.add(Integer.parseInt(in.nextLine()));
+                }
+            } else {
+                for (int i = 0; i < n; i++) {
+                    processNames.add(in.nextLine());
+                    processTimes.add(Integer.parseInt(in.nextLine()));
+                }
             }
             for (int i = 0; i < processTimes.size(); i++) {
                 completedTimes.add(0);
@@ -73,24 +78,80 @@ public class RR2 {
                 if (sum == 0)
                     done = true;
             }
-            System.out.println("RR-2 Scheduling Algorithm");
-            table.print();
-            System.out.println("Average Turnaround Time");
-            System.out.print("= (");
+            if (n == 0) {
+                table.print();
+                System.out.println("Average Turnaround Time");
+                System.out.print("= (");
+            }
             sum = 0;
             for (int i = 0; i < completedTimes.size(); i++) {
                 sum += completedTimes.get(i);
-                System.out.print(completedTimes.get(i));
-                if (i < completedTimes.size() - 1)
-                    System.out.print(" + ");
-                else
-                    System.out.println(") / " + completedTimes.size());
+                if (n == 0) {
+                    System.out.print(completedTimes.get(i));
+                    if (i < completedTimes.size() - 1)
+                        System.out.print(" + ");
+                    else
+                        System.out.println(") / " + completedTimes.size());
+                }
             }
             double averageTurnaroundTime = (sum * 1.0) / processTimes.size();
-            System.out.printf("= %.2f ms\n", averageTurnaroundTime);
+            // do not print for simulation
+            if (n == 0)
+                System.out.printf("= %.2f ms\n", averageTurnaroundTime);
             in.close();
+            return averageTurnaroundTime;
         } catch (Exception e) {
             System.out.println(e);
+        }
+        return -1;
+    }
+
+    public static int menu() {
+        Scanner in = new Scanner(System.in);
+        int option;
+        System.out.println("Select from the following options:");
+        System.out.println("1. Simulate 20 trials with 5 jobs");
+        System.out.println("2. Simulate 20 trials with 10 jobs");
+        System.out.println("3. Simulate 20 trials with 15 jobs");
+        System.out.println("4. Exit\n");
+        System.out.print("> ");
+        option = in.nextInt();
+        in.close();
+        return option;
+    }
+
+    public static void simulate(int n) {
+        File file;
+        int runs = 20;
+        double sum = 0, averageTurnaroundTime;
+        for (int i = 0; i < runs; i++) {
+            file = new File("../input/jobs" + (i + 1) + ".txt");
+            sum += rr2(file, n);
+        }
+        averageTurnaroundTime = sum / runs;
+        System.out.println("Average Turnaround Time for " + runs + " trials with " + n + " jobs");
+        System.err.printf("= %.2f ms\n", averageTurnaroundTime);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("RR-2 Scheduling Algorithm");
+        if (args.length > 0) {
+            rr2(new File(args[0]), 0);
+        } else {
+            int option = menu();
+            switch (option) {
+                case 1:
+                    simulate(5);
+                    break;
+                case 2:
+                    simulate(10);
+                    break;
+                case 3:
+                    simulate(15);
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
